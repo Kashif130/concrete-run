7d6a4449afc9403e533c144b2dcc9a8c81255902
Kashif130 <54382276+Kashif130@users.noreply.github.com>
2026-05-03 19:18:59 +0500

    Delete app/page.tsx

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ color: 'white', textAlign: 'center', padding: 50 }}>
-      <h1>🗿 Concrete Run</h1>
-      <p>Welcome to the Moai Survival Game</p>
-      <a href="/game" style={{ color: '#00f5ff' }}>Play Game</a>
-    </div>
-  );
-}
